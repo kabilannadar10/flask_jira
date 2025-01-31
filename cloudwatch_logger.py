478ca725cb673ff8f19997c13592afda1fc34322
Kabilan Nadar <144523195+kabilannadar@users.noreply.github.com>
2025-01-31 22:10:02 +0530

    Update cloudwatch_logger.py

--- cloudwatch_logger.py
+++ cloudwatch_logger.py
@@ -6,15 +6,15 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 # Log to EC2 (local file)
-file_handler = logging.FileHandler('/var/log/myapp.log')
+file_handler = logging.FileHandler('/home/ubuntu/logs/app.log')
 file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
 logger.addHandler(file_handler)
 
 # Log to AWS CloudWatch
 logger.addHandler(watchtower.CloudWatchLogHandler(
-    log_group="MyAppLogs",
-    stream_name="MyAppStream",
+    log_group="JiraLogs",
+    stream_name="JiraStream",
     boto3_session=boto3.Session()
 ))
 
-logger.info("✅ Logging system initialized!")
+logger.info("Logging system initialized!")
